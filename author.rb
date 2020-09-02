4343bd004d024511d6bd98abfa54db5556f69754
Adam <adamadolfo8@gmail.com>
2020-09-02 18:44:02 -0400

    completed all labs

--- author.rb
+++ author.rb
@@ -1,1 +1,52 @@
+class Author
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        #goes through Book checks if book belongs to this (self) author
+        Book.all.select {|inst_book| inst_book.author == self}
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, self, word_count)
+    end
+
+    def total_words
+        total = 0
+        self.books.select {|inst_book| total += inst_book.word_count}
+        total
+    end
+
+    def self.most_words
+        ##look through books
+        ##look at books word counts
+        ##compare the word counts with other authors
+        
+        winnerbook = Book.all.max do 
+            |inst_book1, inst_book2| inst_book1.word_count <=> inst_book2.word_count
+        end
+        winnerbook.author
+
+    end
+
+
+
+
+
+
+
+
+
+end
 
--- console.rb
+++ console.rb
@@ -2,6 +2,27 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
-binding.pry
+jk = Author.new("jk")
+george_rr = Author.new("george")
+harry1 = jk.write_book("harry potter", 35000)
+got = george_rr.write_book("oiaf", 30000)
+
+
+# binding.pry
+# puts "author knows its books"
+# puts jk.books == [harry1]
+
+
+
+p Author.most_words == jk
+# puts "book knows its word count"
+# p book1.word_count == 35000
+# puts "book knows it's author"
+# puts book1.author == "jk"
+
+
+
+
+# binding.pry
 
 
